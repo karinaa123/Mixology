15f511dea3724f3b1893fda04e18bd30b35f1578
karinaa123 <143716045+karinaa123@users.noreply.github.com>
2025-11-20 11:37:19 -0500

    bug fix

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ class BottleDetectionTrainer:
 
 def main():
     # Setup
-    DATASET_PATH = './anaconda3/envs/kdThesisEnv/mixology/Mixology/bottles-1'
+    DATASET_PATH = 'home/anaconda3/envs/kdThesisEnv/mixology/Mixology/bottles-1'
     DATA_YAML = f'{DATASET_PATH}/data.yaml'
 
     # Verify dataset
